056d6ae204635d9c46490030ca225869f5c4160b
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-28 11:03:11 -0500

    printf01

--- _printf.c
+++ _printf.c
@@ -1,47 +1,50 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "holberton.h"
-
+/**
+ * _printf - print anything
+ * @format: arguments
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-   va_list arguments;
-   const char *p;
-   char *str;
-   int x;
+	va_list arguments;
+	const char *p;
+	char *str;
+	int x;
 
-   va_start(arguments, format);
-   int num = 0;
+	va_start(arguments, format);
+	int num = 0;
 
-   for (p = format; *p; p++)
-   {
-      if (*p != '%')
-      {
-         putchar(*p);
-         num++;
-         continue;
-      }
-      switch (*++p)
-      {
-        case 's':
-            for (str = va_arg(arguments, char *); *str; str++)
-	    {
-                printf("%s", *str);
-                num++;
-            }
-            break;
-        case 'c':
-            x = va_arg(arguments, int);
-            printf("%c", x);
-            num++;
-            break;
-        default:
-            putchar(*p);
-            num++;
-            break;
-      }
-     
-   }
-   va_end(arguments);
-   
-   return (num);
+	for (p = format; *p; p++)
+	{
+		if (*p != '%')
+		{
+			putchar(*p);
+			num++;
+			continue;
+		}
+		switch (*++p)
+		{
+			case 's':
+				for (str = va_arg(arguments, char *); *str; str++)
+				{
+					printf("%s", *str);
+					num++;
+				}
+				break;
+			case 'c':
+				x = va_arg(arguments, int);
+				printf("%c", x);
+				num++;
+				break;
+			default:
+				putchar(*p);
+				num++;
+				break;
+		}
+
+	}
+va_end(arguments);
+return (num);
 }
